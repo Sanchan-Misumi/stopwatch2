05bda02f273059e54e81f4032f92c8cf820191f2
Maho Misumi <maho.smile606@gmail.com>
2017-11-01 15:12:07 +0900

    こっち

--- timer2/ViewController.swift
+++ timer2/ViewController.swift
@@ -9,15 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func start () {
+        if !timer.isValid {
+            
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(self.up),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+        
+    }
+    
+    @IBAction func stop () {
+        if timer.isValid{
+            
+            timer.invalidate()
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @objc func up(){
+        
+        count = count + 0.01
+        
+        label.text = String(format: "%.2f", count)
+        
+        
+        
     }
 
 
